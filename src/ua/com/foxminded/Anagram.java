be17345dfad0563683e47838a9b9300e713d8ac0
Oleksandr Maister <maister.alexander@gmail.com>
2019-02-21 00:02:32 +0200

    Create separate class Anagram

---
+++ src/ua/com/foxminded/Anagram.java
@@ -0,0 +1,38 @@
+package ua.com.foxminded;
+
+public class Anagram {
+	
+    public String makeAnagram(String sentence) {
+    	return reverseString(sentence);
+    }
+	 
+    private String reverseWord(String word) {
+        char[] inputArr = word.toCharArray();
+	    int begin = 0;
+	    int end = inputArr.length-1;
+	    char temp;
+	    while(end>begin) {
+		    if(Character.isLetter(inputArr[begin]) && Character.isLetter(inputArr[end])) {
+			    temp = inputArr[begin];
+			    inputArr[begin] = inputArr[end];
+			    inputArr[end] = temp;
+			    begin++;
+			    end--;
+			} else if(!Character.isLetter(inputArr[begin])) {
+			    begin++;
+			} else if (!Character.isLetter(inputArr[end])) {
+			    end--;
+			}
+		}
+		return new String(inputArr);
+    }
+     
+    private String reverseString(String string) {
+    	String[] wordArr = string.split("\\u0020");
+    	String outputArr = "";
+    	for(String item : wordArr) {
+    	    outputArr += reverseWord(item) + " ";
+    	}
+    	return outputArr;
+    }
+}
---
+++ src/ua/com/foxminded/Main.java
@@ -0,0 +1,18 @@
+package ua.com.foxminded;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+	    Anagram anagram = new Anagram();
+	    
+	    System.out.println("Enter the string: ");
+	    
+	    Scanner scanner = new Scanner(System.in);
+	    String inputString = scanner.nextLine();
+	    
+	    System.out.println(inputString + " => " + anagram.makeAnagram(inputString));
+	}
+
+}

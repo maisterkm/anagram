8c5f6b680d896183a2176ec8bbc68ed60f405749
Oleksandr Maister <maister.alexander@gmail.com>
2019-02-21 00:19:48 +0200

    Correct indent

--- src/ua/com/foxminded/Anagram.java
+++ src/ua/com/foxminded/Anagram.java
@@ -3,28 +3,28 @@ package ua.com.foxminded;
 public class Anagram {
 	
     public String makeAnagram(String sentence) {
-    	return reverseString(sentence);
+        return reverseString(sentence);
     }
 	 
     private String reverseWord(String word) {
         char[] inputArr = word.toCharArray();
-	    int begin = 0;
-	    int end = inputArr.length-1;
-	    char temp;
-	    while(end>begin) {
-		    if(Character.isLetter(inputArr[begin]) && Character.isLetter(inputArr[end])) {
+        int begin = 0;
+        int end = inputArr.length-1;
+        char temp;
+        while(end>begin) {
+            if(Character.isLetter(inputArr[begin]) && Character.isLetter(inputArr[end])) {
 			    temp = inputArr[begin];
 			    inputArr[begin] = inputArr[end];
 			    inputArr[end] = temp;
 			    begin++;
 			    end--;
-			} else if(!Character.isLetter(inputArr[begin])) {
+            } else if(!Character.isLetter(inputArr[begin])) {
 			    begin++;
-			} else if (!Character.isLetter(inputArr[end])) {
+            } else if (!Character.isLetter(inputArr[end])) {
 			    end--;
-			}
-		}
-		return new String(inputArr);
+            }
+        }
+        return new String(inputArr);
     }
      
     private String reverseString(String string) {
@@ -32,7 +32,7 @@ public class Anagram {
     	String outputArr = "";
     	for(String item : wordArr) {
     	    outputArr += reverseWord(item) + " ";
-    	}
-    	return outputArr;
-    }
+        }
+        return outputArr;
+        }
 }

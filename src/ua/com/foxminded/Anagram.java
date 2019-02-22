d6b2d7a315ad20f2ebf657f5fcb8da56dc8ce15f
Oleksandr Maister <maister.alexander@gmail.com>
2019-02-22 18:33:48 +0200

    Remove empty lines and method reverseString

--- src/ua/com/foxminded/Anagram.java
+++ src/ua/com/foxminded/Anagram.java
@@ -1,38 +1,33 @@
 package ua.com.foxminded;
 
 public class Anagram {
-	
     public String makeAnagram(String sentence) {
-        return reverseString(sentence);
-    }
-	 
-    private String reverseWord(String word) {
-        char[] inputArr = word.toCharArray();
-        int begin = 0;
-        int end = inputArr.length-1;
-        char temp;
-        while(end>begin) {
-            if(Character.isLetter(inputArr[begin]) && Character.isLetter(inputArr[end])) {
-			    temp = inputArr[begin];
-			    inputArr[begin] = inputArr[end];
-			    inputArr[end] = temp;
-			    begin++;
-			    end--;
-            } else if(!Character.isLetter(inputArr[begin])) {
-			    begin++;
-            } else if (!Character.isLetter(inputArr[end])) {
-			    end--;
-            }
+        String[] wordArr = sentence.split("\\u0020");
+        String anagram = "";
+        for(String item : wordArr) {
+            anagram += reverseWord(item) + " ";
         }
-        return new String(inputArr);
+        return anagram;
     }
      
-    private String reverseString(String string) {
-    	String[] wordArr = string.split("\\u0020");
-    	String outputArr = "";
-    	for(String item : wordArr) {
-    	    outputArr += reverseWord(item) + " ";
+    private String reverseWord(String word) {
+        char[] characters = word.toCharArray();
+        int begin = 0;
+        int end = characters.length-1;
+        char temp;
+        while(end>begin) {
+            if(Character.isLetter(characters[begin]) && Character.isLetter(characters[end])) {
+                temp = characters[begin];
+                characters[begin] = characters[end];
+                characters[end] = temp;
+                begin++;
+                end--;
+            } else if(!Character.isLetter(characters[begin])) {
+                begin++;
+            } else if (!Character.isLetter(characters[end])) {
+                end--;
+            }
         }
-        return outputArr;
+        return new String(characters);
     }
 }
--- src/ua/com/foxminded/Main.java
+++ src/ua/com/foxminded/Main.java
@@ -3,16 +3,12 @@ package ua.com.foxminded;
 import java.util.Scanner;
 
 public class Main {
-
-	public static void main(String[] args) {
-	    Anagram anagram = new Anagram();
-	    
-	    System.out.println("Enter the string: ");
-	    
-	    Scanner scanner = new Scanner(System.in);
-	    String inputString = scanner.nextLine();
-	    
-	    System.out.println(inputString + " => " + anagram.makeAnagram(inputString));
-	}
+    public static void main(String[] args) {
+        Anagram anagram = new Anagram();
+        System.out.println("Enter the string: ");
+        Scanner scanner = new Scanner(System.in);
+        String inputString = scanner.nextLine();
+        System.out.println(inputString + " => " + anagram.makeAnagram(inputString));
+    }
 
 }

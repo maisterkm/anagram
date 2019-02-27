e14aaace920bd9cd20a8d5b7fcab59106b0d9b85
Oleksandr Maister <maister.alexander@gmail.com>
2019-02-27 17:10:10 +0200

    Add JUnit Test Case

---
+++ src/test/java/ua/com/foxminded/AnagramTest.java
@@ -0,0 +1,14 @@
+package ua.com.foxminded;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AnagramTest {
+
+    @Test
+    public void testMakeAnagram() {
+//        fail("Not yet implemented");
+    }
+
+}

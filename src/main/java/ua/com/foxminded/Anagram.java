b138cdf782c653e1f75a7d980378e57fc03cde8f
Oleksandr Maister <maister.alexander@gmail.com>
2019-03-02 13:25:57 +0200

    Add test methods

--- src/main/java/ua/com/foxminded/Anagram.java
+++ src/main/java/ua/com/foxminded/Anagram.java
@@ -1,11 +1,22 @@
 package ua.com.foxminded;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Anagram {
     public String makeAnagram(String sentence) {
-        String[] wordArr = sentence.split("\\u0020");
+        List<String> w = new ArrayList<String>();
+        String[] arrayOfWord = sentence.split("\\s+");
+        ArrayList<String> listOfWord = new ArrayList<String>(Arrays.asList(arrayOfWord));
+        
         String anagram = "";
-        for(String item : wordArr) {
-            anagram += reverseWord(item) + " ";
+        for(String item : listOfWord) {
+            if(item != listOfWord.get(listOfWord.size()-1)) {
+                anagram += reverseWord(item) + " ";
+            } else {
+                anagram += reverseWord(item);
+            }
         }
         return anagram;
     }
--- src/test/java/ua/com/foxminded/AnagramTest.java
+++ src/test/java/ua/com/foxminded/AnagramTest.java
@@ -2,13 +2,56 @@ package ua.com.foxminded;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class AnagramTest {
 
+    Anagram anagram;
+    
+    @Before
+    public void before() {
+        anagram = new Anagram();
+    }
+    
     @Test
-    public void testMakeAnagram() {
-//        fail("Not yet implemented");
+    public void stringShouldBeReversed() {
+        assertEquals("dcba hgfe", anagram.makeAnagram("abcd efgh"));
+        assertEquals("a1bcd efg!h", anagram.makeAnagram("d1cba hgf!e"));
+    }
+    
+    @Test
+    public void stringShouldNotBeEmpty() {
+        String string = "dcba hgfe";
+        assertFalse(string.isEmpty());
+    }
+    
+    @Test
+    public void stringShouldNotBeNull() {
+        String string = "dcba hgfe";
+        assertNotNull(string);
+    }
+    
+    @Test
+    public void stringShouldBeReversedWithSingleWhitespace() {
+        assertEquals("dcba hgfe", anagram.makeAnagram("abcd efgh"));
+    }
+    
+    @Test
+    public void stringShouldBeReversedWithMultipleWhitespace() {
+        assertEquals("dcba hgfe", anagram.makeAnagram("abcd     efgh"));
+    }
+    
+    @Test
+    public void stringShouldBeReversedWithCR() {
+        assertEquals("dcba hgfe", anagram.makeAnagram("abcd \refgh"));
+        assertEquals("dcba hgfe", anagram.makeAnagram("abcd \r\refgh"));
+    }
+    
+    @Test
+    public void stringShouldBeReversedWithLF() {
+        assertEquals("dcba hgfe", anagram.makeAnagram("abcd \nefgh"));
+        assertEquals("dcba hgfe", anagram.makeAnagram("abcd \n\nefgh"));
     }
 
 }
